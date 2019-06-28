d2c20bc77c9dbe3a45cab4d734dd84c48bbc7e3e
GhostlyCoding <49044974+Ghostly-Coding@users.noreply.github.com>
2019-06-28 09:49:48 -0500

    Create physics.js

---
+++ Js Physics/Newton's Second Law/physics.js
@@ -0,0 +1,11 @@
+function rectMass(unitMass,area){
+	return (unitMass * area) / 10;
+}
+
+function circMass(unitMass,radius){
+	return (unitMass * (radius * Math.PI)) / 10;
+}
+
+function calcAccel(force,mass){
+	return new vector(force.x / mass, force.y / mass);
+}
