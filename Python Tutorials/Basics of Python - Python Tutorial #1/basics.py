5a890c2921caa26bd6b17642cf75d186890f9a7d
GhostlyCoding <49044974+Ghostly-Coding@users.noreply.github.com>
2019-09-22 20:12:54 -0500

    Create basics.py

---
+++ Python Tutorials/Basics of Python - Python Tutorial #1/basics.py
@@ -0,0 +1,28 @@
+print("Hello, World!") # prints Hello, World!
+#########################################################
+hi = "Hello, World!"
+print(hi) # prints the value of hi, which is Hello, World!
+#########################################################
+x = 5
+y = 4
+z = x + y # get the value of x and the value of y and add the two together
+print(z)
+#########################################################
+x = 23
+x += 1 # set x to the old value of x and add 1 to it
+print(x)
+#########################################################
+hi = ["HI ", "BOB"]
+hi[0] = "gl hf " # set 0th value of hi to "gl hf"
+hi = hi[0] + hi[1] # concatentates hi index 0's value with hi index 1's value
+print(hi)
+#########################################################
+hi = "Hi "
+hi += "Bob" # concatenating Bob with Hi to make the new value "Hi Bob"
+print(hi)
+#########################################################
+# compile code with: python basics.py
+# or if on windows and you just installed python
+# py basics.py
+# or
+# C:\Windows\python.exe basics.py
